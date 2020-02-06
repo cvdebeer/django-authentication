b0c0c8703d7899447c81dba017d5ab71ba81c4a0
Claudia De Beer <claudiadiebeer@gmail.com>
2020-02-06 19:49:17 +0000

    authorisation login_required

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 # Create your views here.
@@ -10,6 +11,7 @@ def index(request):
     return render(request, 'index.html')
 
 
+@login_required
 def logout(request):
     '''Log the user out'''
     auth.logout(request)
@@ -19,6 +21,8 @@ def logout(request):
 
 def login(request):
     '''Return a login page '''
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -28,7 +32,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, 'You have successfully logged in!')
-
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, 'Your username or password is incorrect')
     else:
